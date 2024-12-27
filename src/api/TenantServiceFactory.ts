c2583bdca79f543ed02dacf4421f9848d318cb0f
Daniel Le <nguyenngoc.le@dzhintl.com>
2024-12-27 11:48:49 +0800

    Refactor TenantServiceFactory to use static instances for MySQLTenantStorage and FileTenantStorage

--- src/api/TenantServiceFactory.ts
+++ src/api/TenantServiceFactory.ts
@@ -4,14 +4,18 @@ import { FileTenantStorage } from './FileTenantStorage';
 import { ITenantStorage } from './ITenantStorage';
 
 export class TenantServiceFactory {
+    // Keep MySQLTenantStorage and FileTenantStorage as static so that we don't have to keep creating new instances of these classes
+    static mysqlTenantStorage = new MySQLTenantStorage();
+    static fileTenantStorage = new FileTenantStorage();
+    
     static createTenantService(): ITenantStorage {
         const storageType = process.env.STORAGE_TYPE || 'file';
         switch (storageType) {
             case 'mysql':
-                return new MySQLTenantStorage();
+                return this.mysqlTenantStorage;
             case 'file':
             default:
-                return new FileTenantStorage();
+                return this.fileTenantStorage;
         }
     }
 }
